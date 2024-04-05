3d8a684391a3029d7d06ea77957381447cd028ac
Matheus Gois <matheus.gois@btgpactual.com>
2024-04-05 11:48:49 -0300

    Initial Commit

---
+++ Tests-CI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Tests-CI
+//
+//  Created by Matheus Gois on 05/04/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
